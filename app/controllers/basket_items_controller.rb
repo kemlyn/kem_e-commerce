7f13dc136f0b86180114895d6b27a03e291387ed
Kemlyn Reid <kemlyn@reel.tech>
2018-04-09 10:26:23 +0100

    KEM-50 product baskets and total prices for everything in basket

--- app/controllers/basket_items_controller.rb
+++ app/controllers/basket_items_controller.rb
@@ -1,5 +1,35 @@
 class BasketItemsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @basket_items = current_user.basket_items
+  end
+
+  def update
+    @basket_item = BasketItem.find(params[:id])
+    if @basket_item[:quantity].zero?
+      @basket_item.destroy
+      redirect_to basket_items_path
+    else
+      @basket_item.update(basket_params)
+    end
+  end
+
+  def create
+    @basket_item = current_user.basket_items.new(basket_params)
+    @basket_item.save
+    redirect_to basket_items_path
+  end
+
+  def destroy
+    @basket_item = BasketItem.find(params[:id])
+    @basket_item.destroy
+    redirect_to basket_items_path
+  end
+
+  protected
+
+  def basket_params
+    params.require(:basket_item).permit(:product_id, :quantity)
   end
 end
