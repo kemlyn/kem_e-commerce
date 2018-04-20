8b186d4485c512ddaa426129c09ea24e40fd589b
Kemlyn Reid <kemlyn@reel.tech>
2018-04-20 15:50:32 +0100

    KEM-38 neatened up general coding

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,11 +2,7 @@ class OrdersController < ApplicationController
   def index
     @orders = current_user.orders
   end
-
-  def show
-    @order = current_user.orders.find(params[:id])
-  end
-
+  
   def new
     @order = current_user.orders.new
   end
